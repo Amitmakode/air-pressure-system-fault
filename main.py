db8e9a6ced6ce4f7960eaacf83ae92305b1b208e
amitmakode <amitmakode007@gmail.com>
2022-12-04 20:09:49 +0530

    Update

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from sensor.logger import logging
 from sensor.exception import SensorException
-from sensor.utils import get_collection_as_dataframe
 import sys,os
-from sensor.entity import config_entity
-from sensor.components.data_ingestion import DataIngestion
-
-def test_logger_and_exception():
-     try:
-          result = 3/0
-          print (result)
-          pass
-     except Exception as e:
-          raise SensorException(e, sys)
+ 
 
 
 if __name__ == "__main__":
