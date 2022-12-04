5d43fc3285f1792b28d3b3b7e611e46191965a6e
amitmakode <amitmakode007@gmail.com>
2022-12-05 00:21:31 +0530

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sensor.logger import logging
 from sensor.exception import SensorException
+from sensor.utils import get_collection_as_dataframe
 import sys,os
  
 
@@ -7,6 +8,5 @@ import sys,os
 if __name__ == "__main__":
      try:
           get_collection_as_dataframe(database_name="aps", collection_name="sensor")
-          
      except Exception as e:
           print(e)
