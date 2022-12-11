0fd9cb6afba844e1660072fab3c4c1e9f5465194
amitmakode <amitmakode007@gmail.com>
2022-12-12 01:33:52 +0530

    update validation file

--- main.py
+++ main.py
@@ -17,3 +17,18 @@ if __name__=="__main__":
           print(data_ingestion.initiate_data_ingestion())
      except Exception as e:
           print(e)
+
+
+
+"""from sensor.logger import logging
+from sensor.exception import SensorException
+from sensor.utils import get_collection_as_dataframe
+import sys,os
+ 
+
+
+if __name__ == "__main__":
+     try:
+          get_collection_as_dataframe(database_name="aps", collection_name="sensor")
+     except Exception as e:
+          print(e)   """       
