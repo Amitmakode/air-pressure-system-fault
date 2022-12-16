ce4e863a80d97b53280c9c6b4763159814f40ca2
amitmakode <amitmakode007@gmail.com>
2022-12-16 21:42:16 +0530

    add

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ if __name__ == "__main__":
      try:
           get_collection_as_dataframe(database_name="aps", collection_name="sensor")
      except Exception as e:
-          print(e)   """     
+          print(e)"""    
