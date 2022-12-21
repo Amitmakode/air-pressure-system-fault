ef06dfee8b1a50a96b246548f2f7e50a0d3bca06
amitmakode <amitmakode007@gmail.com>
2022-12-22 00:04:51 +0530

    aslkdj

--- data_dump.py
+++ data_dump.py
@@ -1,28 +1,23 @@
 import pymongo
 import pandas as pd
 import json
+from sensor.config import mongo_client
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+DATA_FILE_PATH= "/config/workspace/aps_failure_training_set1.csv"
+DATABASE_NAME= "aps"
+COLLECTION_NAME= "sensor"
 
-Datafile_path = "/config/workspace/aps_failure_training_set1.csv"
-Database_name = "aps"
-Collection_name = "sensor"
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
 
-
-if __name__ == "__main__":
-    df = pd.read_csv(Datafile_path)
-    print(f"Rows and Columns: {df.shape} ")
-
-    #convert data frame into json formate for dump data in mongoDB
-    df.reset_index(drop = True, inplace = True )
+    #Convert dataframe to json so that we can dump these record in mongo db
+    df.reset_index(drop=True,inplace=True)
 
     json_record = list(json.loads(df.T.to_json()).values())
-    print(json_record [0])
-
-    #insert converted record to mongoDB
-    client[Database_name][Collection_name].insert_many(json_record)
-
+    print(json_record[0])
+    #insert converted json record to mongo db
+    mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
     
 
 
--- sensor/config.py
+++ sensor/config.py
@@ -3,10 +3,9 @@ import pandas as pd
 import json
 from dataclasses import dataclass
 import os
-@dataclass
-
 # Provide the mongodb localhost url to connect python to mongodb.
 
+@dataclass
 class EnvironmentVariable:
     mongo_db_url:str = os.getenv("MONGO_DB_URL")
     aws_access_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
@@ -14,16 +13,9 @@ class EnvironmentVariable:
 
 # this for future project
 # this dictionary for target column encoding but we are not using this we are using lable encoding instead of this dictionary
-"""TARGET_COLUMN_MAPPING = {
-    "pos":1,
-    "neg":0
-}
-"""
+#TARGET_COLUMN_MAPPING = {"pos":1,"neg":0}
 
 
 env_var = EnvironmentVariable()
-
 mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
-
-
 TARGET_COLUMN = "class"
